14aca9ef135bf2289f1aa314896c2cf47bad7889
Tryck <theeburntgamer@gmail.com>
2018-06-29 21:01:01 -0500

    Movement script and Dialogue

---
+++ Assets/Scripts/GameObjects/IBscr.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IBscr : MonoBehaviour {
+
+    public string OneLiner;
+    public List<string> MultiLiner;
+
+    public bool isOneLiner;
+    public bool isDia;
+
+    Text diaText;
+    // Use this for initialization
+	void Start () {
+        diaText = GameObject.Find("DiaText").GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        if(isDia == true)
+        {
+            DialogueInteraction();
+        }
+
+    }
+
+    public void DialogueInteraction()
+    {
+        if(isOneLiner == true)
+        {
+            diaText.text = OneLiner;
+            if (Input.GetKeyDown(KeyCode.Tab))
+            {
+                diaText.text = "";
+                isDia = false;
+            }
+            else if(isOneLiner == false)
+            {
+                
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "IB")
+        {
+            if(Input.GetKeyDown(KeyCode.F))  //"IB" Means Interactable
+            {
+                other.GetComponent<IBscr>().isDia = true;
+            }
+        }
+    }
+}
